7a370059458341c3cd6481ce3aadc17a8c089b66
jolene <jolliu26@bergen.org>
2025-06-09 14:20:58 -0400

    audio wip
    wip

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("----------- Audio Source -----------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+
+    [Header("----------- Audio Clip -----------")]
+    public AudioClip background;
+    public AudioClip start;
+    public AudioClip combine;
+    public audioclip restart;
+    
+}
